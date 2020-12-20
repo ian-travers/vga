08497b334be38cb3dfbc2d0f29fbd4099ff79463
Ian Travers <mr.itpe@gmail.com>
2020-12-20 10:44:26 +0300

    Extract layout blade component (further bc)

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Video Game Aggregator</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css', 'build') }}">
+</head>
+<body class="antialiased">
+
+{{ $slot }}
+
+<script src="{{ mix('js/app.js', 'build') }}"></script>
+</body>
+</html>
+
