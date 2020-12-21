b0d6264709f2ac8284fa529f57169c682801c18c
Ian Travers <mr.itpe@gmail.com>
2020-12-21 15:18:42 +0300

    Create a dummy normal game card

---
+++ resources/views/components/game-card-normal.blade.php
@@ -0,0 +1,20 @@
+<div class="game mt-6">
+    <div class="relative inline-block">
+        <a href="#">
+            <img
+                class="hover:opacity-75 transition ease-in-out duration-300"
+                src="https://via.placeholder.com/150"
+                alt="game cover"
+            >
+        </a>
+        <div
+            class="absolute w-16 h-16 bg-gray-800 rounded-full"
+            style="right: -20px; bottom: -20px;"
+        >
+            <div class="flex items-center justify-center font-semibold text-xs  h-full">15%</div>
+        </div>
+    </div>
+    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Game Title as long as it could be</a>
+    <div class="text-gray-400 mt-1">Platform list</div>
+</div>
+
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-    Content
-
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-bold">Popular Games</h2>
+        <div class="popular-games grid grid-cols-6 text-sm gap-12 border-b border-gray-800 pb-12">
+            <x-game-card-normal></x-game-card-normal>
+        </div>
+    </div>
 </x-layout>
 
