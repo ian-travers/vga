42e8918c565584e07ed9b190bbd7cb11008351a3
Ian Travers <mr.itpe@gmail.com>
2020-12-22 16:22:40 +0300

    Add filter to first_release_date

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -2,19 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 
 class GamesController extends Controller
 {
     public function index()
     {
+        $before = Carbon::now()->subMonths(2)->timestamp;
+        $after = Carbon::now()->addMonths(2)->timestamp;
+
         $popularGames = Http::withHeaders([
             'Client-ID' => config('services.igdb.client_id'),
             'Authorization' => config('services.igdb.authorization')
         ])
             ->withBody("
 fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, slug;
-  where platforms = (48,49,130,6) & total_rating_count > 5;
+  where platforms = (48,49,130,6) & total_rating_count > 5
+    & (first_release_date >= {$before} & first_release_date < {$after});
   sort total_rating_count desc;
   limit 4;
             ", 'text/plain')
