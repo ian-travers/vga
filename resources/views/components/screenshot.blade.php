04c8ae01655d7a96d2e62a0486d3f2be23fc9fe0
Ian Travers <mr.itpe@gmail.com>
2020-12-22 10:23:14 +0300

    Style a single game page

---
+++ resources/views/components/screenshot.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <a href="#">
+        <img
+            src="https://via.placeholder.com/600x400"
+            class="hover:opacity-75 transition ease-in-out duration-300"
+            alt="screenshot"
+        >
+    </a>
+</div>
+
