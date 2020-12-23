7db7eae3921924549e1cc466e8309e71d48589b2
Ian Travers <mr.itpe@gmail.com>
2020-12-23 12:14:21 +0300

    Fix image width for small game card (when a title is too long)

--- resources/views/components/game-card-small.blade.php
+++ resources/views/components/game-card-small.blade.php
@@ -1,13 +1,15 @@
 @props(['name', 'cover', 'releaseDate'])
 
 <div class="game flex justify-center md:justify-start">
-    <a href="#">
-        <img
-            class="hover:opacity-75 transition ease-in-out duration-300 w-30"
-            src="{{ Str::replaceFirst('thumb', 'cover_small', $cover) }}"
-            alt="game cover"
-        >
-    </a>
+    <div class="flex-none">
+        <a href="#">
+            <img
+                class="hover:opacity-75 transition ease-in-out duration-300 w-30"
+                src="{{ Str::replaceFirst('thumb', 'cover_small', $cover) }}"
+                alt="game cover"
+            >
+        </a>
+    </div>
     <div class="ml-4">
         <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-2">{{ $name }}</a>
         <div class="text-gray-400 text-sm mt-1">
